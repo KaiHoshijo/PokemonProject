ca66db63278ca8fa0c2b0e4679bbacedaba8b9a8
KaiHoshijo <34174416+KaiHoshijo@users.noreply.github.com>
2019-12-11 21:51:05 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        // creates the pokemon charmander and raichu
         Pokemon charmander = new Pokemon(false, false, 30, "Charmander");
         Pokemon raichu = new Pokemon(false, false, 90, "Raichu");
+        // creates the new Trainer named Kai (totally a randomly generated name)
         Trainer Kai = new Trainer("Kai", "B", "Hoshijo", false);
 
         System.out.println(charmander.compare(raichu));
