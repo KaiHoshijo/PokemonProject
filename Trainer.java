d826b95c48f197f7ceeb3ee0fb710b14010cc241
KaiHoshijo <34174416+KaiHoshijo@users.noreply.github.com>
2019-12-11 21:53:26 -0700

    Update Trainer.java

--- Trainer.java
+++ Trainer.java
@@ -1,16 +1,20 @@
 public class Trainer {
+  // fields
   private String firstName;
   private String middleInitial;
   private String lastName;
   private boolean hasPlayed;
 
+  // didn't want to leave it as just fields
   public Trainer(String firstName, String mI, String lastName, boolean hasPlayed) {
+    // initializes the fields with given parameters
     this.firstName = firstName;
     this.middleInitial = mI;
     this.lastName = lastName;
     this.hasPlayed = hasPlayed;
   }
 
+  // to String method for the Trainer class
   public String toString() {
     String yesPlayed = this.hasPlayed ? "" : " not";
     return firstName+ " " + middleInitial + " " + lastName + " has" + yesPlayed + " played before!";
